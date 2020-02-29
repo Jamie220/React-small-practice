ad62552f727a7010227ecd848efcfaf3f7199e45
Wang Weijing <you@example.com>
2020-03-01 00:34:38 +1100

    update useEffect

--- src/Components/ReactHooksPractices/ReactHooksPractices.js
+++ src/Components/ReactHooksPractices/ReactHooksPractices.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from './useForm';
+import { useFetch } from './useFetch';
 
 
 
 const ReactHooksPractice = () => {
 
-    const [value, handleChange] = useForm({email: "", password:""})
+    const [values, handleChange] = useForm({ email: "", password: "" })
 
+    // useEffect(() => {
+    //     const onMouseMove = e => {
+    //         console.log(e)
+    //     }
+    //     window.addEventListener("mousemove", onMouseMove)
+    //     return () => {
+    //         window.removeEventListener("mousemove", onMouseMove)
+    //     };
+    // }, [])
+
+    const [count, setCount] = useState(()=> JSON.parse(localStorage.getItem("count")));
+    const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`)
+
+
+    //update the data in local storage when it is reflesh will do const [count, setCount] = useState(()=> JSON.parse(localStorage.getItem("count")));
+    useEffect(()=>{
+        localStorage.setItem("count", JSON.stringify(count))
+    })
     return (
         <div style={{ textAlign: 'center' }}>
             <h3>Practical React hooks</h3>
-            <input name="email" type="email" value ={value.email} onChange={handleChange}/>
-            <input name="password" type="password" value={value.password} onChange ={handleChange}/>
+            <input
+                name="email"
+                value={values.email}
+                onChange={handleChange} />
+            <input
+                name="password"
+                type="password"
+                value={values.password}
+                onChange={handleChange} />
+            <div>{!data ? "loading..." : data}</div>
+            <button onClick={() => setCount(c => c + 1)}>increase</button>
         </div>
     )
 }
--- src/Components/ReactHooksPractices/ReactHooksPractices.md
+++ src/Components/ReactHooksPractices/ReactHooksPractices.md
@@ -4,7 +4,7 @@
 
 you can't nest hooks inside loops, conditions, nested functions.
 
-### useState :rocket:
+### useState( ) :rocket:
 
 `useState(()=>10) // computation very expensive`
 
@@ -32,4 +32,32 @@ export default ReactHooksPractice;
 using inline function in `setCount()` will aviod having two updates happen on same time, there is example below:
 ```JavaScript
 <button onClick={() => setCount(currentState => currentState + 1)}>+</button>
+```
+
+### useEffect( ) :cat:
+
+Everytime the component get render or re-render, useEffect() will get to call.
+
+kind of replace `componentDidMount()` and `componentWillUnMount()`
+
+```javascript
+//only will get to call when change the password
+useEffect(()=>{
+    console.log("render")
+}, [values.password])
+//second parameter is called the dependency array that you can pass in
+```
+if didn't put value in `[ ]` so `useEffect()` only called once on the beginning. 
+
+**clean up function:**
+```javascript
+
+useEffect(()=>{
+    console.log("render")
+
+    return () => {
+        console.log("unmount")
+    }
+}, [values.password])
+
 ```
